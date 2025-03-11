fe5fe93c36ea77be57f4c6e251f2145380b5fb31
aysunlol <aysun.kasap@metropolia.fi>
2025-03-11 15:16:36 +0200

    päivitys

--- 01.py
+++ 01.py
@@ -148,7 +148,7 @@ def etsi_puu():
     puun_sijainti = (
         f"select name from airport where iso_country = 'FI' AND name like '%airport%' and name like 'M%' or iso_country = 'FI' AND name like '%airport%' and name like 'S%' or iso_country = 'FI' AND name like '%airport%' and name like 'H%' or iso_country = 'FI' AND name like '%airport%' and name like 'J%'")
     if puun_sijainti in puun_sijainti:
-    print("Löysit puun")
+        print("Löysit puun")
     else:
         print("Puuta ei ole saatavilla tällä lentokentällä")
 
